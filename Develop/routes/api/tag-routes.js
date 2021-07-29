a3acac6067958db40f6e2f784b60f266bdbc94fa
Joshua Gonzalez <jalgonz96@gmail.com>
2021-07-29 19:18:32 -0400

    get route to find all tags

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,6 +6,13 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [Product]
+  }).then(dbRes => {
+    res.json(dbRes)
+  }).catch(err => {
+    res.json(err)
+  }) 
 });
 
 router.get('/:id', (req, res) => {

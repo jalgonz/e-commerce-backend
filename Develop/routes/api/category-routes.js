bf12b8d1db609c0464544f545b017966225d7cb1
Joshua Gonzalez <jalgonz96@gmail.com>
2021-07-29 19:04:45 -0400

    added get route to find by id

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,11 +6,28 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [Product]
+  }).then(dbRes => {
+    res.json(dbRes)
+  }).catch(err => {
+    res.json(err)
+  }) 
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+  }).then(dbRes => {
+    res.json(dbRes)
+  }).catch(err => {
+    res.json(err)
+  }) 
 });
 
 router.post('/', (req, res) => {
